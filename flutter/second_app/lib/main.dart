41791f19de2d1dc43b9feb4166b469e335bfdca7
Durga Sailesh <ndurgasailesh@hotmail.com>
2018-07-20 00:29:49 +1000

    Adding ProductList in stateful widget

--- flutter/second_app/lib/main.dart
+++ flutter/second_app/lib/main.dart
@@ -4,40 +4,49 @@ main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
-    State<StatefulWidget> createState() {
-      // TODO: implement createState
-      return _MyAppState();
-    }
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
 }
 
 class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Car Driving Test'];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Cars List'),
-        ),
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.all(10.00),
-              child: RaisedButton(
-                onPressed: () {},
-                child: Text('Add Car'),
+          appBar: AppBar(
+            title: Text('Cars List'),
+          ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.00),
+                child: RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      _products.add('Demo Test1');
+                    // print(_products);              
+                         });                  
+                  },
+                  child: Text('Add Car'),
+                ),
               ),
-            ),
-            Card(
-              child: Column(
-                children: <Widget>[
-                  Image.asset('assets/car1.jpg'),
-                  Text('My Cars')
-                ],
+              Column(
+                children: _products
+                    .map((element) => Card(
+                          child: Column(
+                            children: <Widget>[
+                              Image.asset('assets/car1.jpg'),
+                              Text(element)
+                            ],
+                          ),
+                        ))
+                    .toList(),
               ),
-            )
-          ],
-        ),
-      ),
+            ],
+          )),
     );
   }
 }
