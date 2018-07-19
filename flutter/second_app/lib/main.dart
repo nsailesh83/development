d32ab57766493c7f59102b0760a166394b469f7b
Durga Sailesh <ndurgasailesh@hotmail.com>
2018-07-19 22:18:42 +1000

    First Application

---
+++ flutter/second_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Users List'),
+      ),
+    ),
+    );
+  }
+}
