5870513e6751812d42a78339c0c56231a3098d60
Durga Sailesh <ndurgasailesh@hotmail.com>
2018-07-19 23:13:26 +1000

    Add Button

--- flutter/second_app/lib/main.dart
+++ flutter/second_app/lib/main.dart
@@ -2,19 +2,34 @@ import 'package:flutter/material.dart';
 
 main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Cars List'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Cars List'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10.00),
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text('Add Car'),
+              ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assets/car1.jpg'),
+                  Text('My Cars')
+                ],
+              ),
+            )
+          ],
+        ),
       ),
-      body: Card(child: Column(children: <Widget>[
-        Image.asset('assets/car1.jpg'),
-        Text('My Cars')
-      ],),),
-    ),
     );
   }
 }
